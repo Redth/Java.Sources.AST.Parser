2306bab85ca9416e37ef2844de537c4b8f2bcf44
Jonathan Dick <jondick@gmail.com>
2018-04-27 11:30:16 -0400

    Remove template stuff

--- src/main/java/com/xamarin/javaastparameternames/MemberInfo.java
+++ src/main/java/com/xamarin/javaastparameternames/MemberInfo.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.xamarin.javaastparameternames;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author redth
- */
 public class MemberInfo {
     public String Name;
     public List<ParamInfo> Parameters = new ArrayList<>();
